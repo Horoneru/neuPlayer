43ecfe440016f41d4f12e3538a64076f74436f5f
Horoneru <horonerudev@gmail.com>
2015-04-14 20:00:44 +0200

    fadeOutWindow now also manages FadeIn transitions
    Changed the name of the class to reflect this change. Also changed it to
    fully support fade-in, with non-1.0 end value support.

--- fadewindow.h
+++ fadewindow.h
@@ -1,5 +1,5 @@
-#ifndef FADEOUTWINDOW_H
-#define FADEOUTWINDOW_H
+#ifndef FADEWINDOW_H
+#define FADEWINDOW_H
 
 #include <QObject>
 #include <QDialog>
@@ -12,18 +12,19 @@
 */
 
 
-class FadeOutWindow : public QObject
+class FadeWindow : public QObject
 {
     Q_OBJECT
 public:
-    explicit FadeOutWindow(QObject *parent = 0);
+    enum FadeMode { FadeOut, FadeIn };
+    explicit FadeWindow(QObject *parent = 0);
     //We're sure to only have windows.
-    FadeOutWindow(QDialog *dialogWidget, const int msecs, QObject *parent);
-    FadeOutWindow(QMainWindow *mainWindowWidget, const int msecs, QObject *parent);
-    FadeOutWindow(QWindow *windowWidget, const int msecs, QObject *parent);
-    FadeOutWindow(QDialog *dialogWidget, QObject *parent);
-    FadeOutWindow(QMainWindow *mainWindowWidget, QObject *parent);
-    FadeOutWindow(QWindow *windowWidget, QObject *parent);
+    FadeWindow(QDialog *dialogWidget, const int msecs, FadeMode = FadeMode::FadeIn, QObject *parent = nullptr);
+    FadeWindow(QMainWindow *mainWindowWidget, const int msecs, FadeMode = FadeMode::FadeIn, QObject *parent = nullptr);
+    FadeWindow(QWindow *windowWidget, const int msecs, FadeMode = FadeMode::FadeIn, QObject *parent = nullptr);
+    FadeWindow(QDialog *dialogWidget, QObject *parent= nullptr);
+    FadeWindow(QMainWindow *mainWindowWidget, QObject *parent = nullptr);
+    FadeWindow(QWindow *windowWidget, QObject *parent = nullptr);
 
     //Duration of animation
     inline void setDuration(const int duration)
@@ -36,7 +37,7 @@ public:
         a_timerEnabled = enabled;
     }
 
-    void start();
+    void start(qreal endValue = 0.0);
 
     inline const bool isFinished() const
     {
@@ -51,10 +52,14 @@ private slots:
     }
 
 private:
+
+    void setMode(QPropertyAnimation *anim, FadeMode mode, qreal endValue);
+    //Attributes
     int a_duration;
     QObject *a_target; //All the windows inherit from QWidget, so to simplify things we're also sure it's a QObject.
+    FadeMode a_mode;
     bool a_timerEnabled;
     bool a_finished;
 };
 
-#endif // FADEOUTWINDOW_H
+#endif // FADEWINDOW_H
